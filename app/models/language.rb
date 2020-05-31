d1af2320f127bda98613b733a32db07366487230
Raghu Betina <raghu@firstdraft.com>
2020-05-31 16:12:47 -0500

    rails g model Language name:string code:string

---
+++ app/models/language.rb
@@ -0,0 +1,12 @@
+# == Schema Information
+#
+# Table name: languages
+#
+#  id         :integer          not null, primary key
+#  code       :string
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Language < ApplicationRecord
+end
